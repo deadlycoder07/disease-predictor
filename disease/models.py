d202daffee763043364fae758adb009a9e130222
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-05-17 11:42:15 +0530

    disease model

--- disease/models.py
+++ disease/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Diseases(models.Model):
+    disease=models.CharField(max_length=50)
+    symptoms=models.CharField(max_lenth=199)
+    recoveryperiod=models.IntegerField()
+    medications=models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=50, null=True)
+    district = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    pincode = models.IntegerField(null=True)
+    
+
