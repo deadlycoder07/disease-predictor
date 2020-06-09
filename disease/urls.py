f9ab2208fa5b716999c317f92a7203756692ede0
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-06-09 17:17:01 +0530

    ui changed

--- disease/urls.py
+++ disease/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 
@@ -11,12 +11,12 @@ urlpatterns = [
     path('checkbyquestion/', views.userquestion, name="userquestion"),
     path('analysis2/', views.qanalysis, name="analysis2"),
     path('analysis/', views.analysis, name="analysis"),
-    path('',views.home,name='index'),
+    path('',views.index,name='index'),
     path('HospitalDashboard/', views.Hhome, name='HospitalDashboard'),
     path('contact/', views.contact, name="contact"),
     path('about/', views.aboutus, name="aboutus"),
     path('privacy/', views.privacy, name="privacy"),
-    
+    re_path(r'^.*\.html', views.pages, name='pages'),
     
     
 ]
