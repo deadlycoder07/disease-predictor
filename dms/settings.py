a11dc09323f4f5dd4ae3810b9b2379fb6bb59af8
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-07-30 20:00:14 +0530

    heroku

--- dms/settings.py
+++ dms/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -202,3 +202,5 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'images')
 
 CORS_ORIGIN_ALLOW_ALL= True
+
+django_heroku.settings(locals())
